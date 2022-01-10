309a96ea1d4189b4012d56e97902ab252bd7feff
Akshay <akshay.2047@gmail.com>
2022-01-09 21:25:50 -0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,21 +1,44 @@
-let shuffleFilters = document.getElementById("shuffleFilters");
+let docShuffleFilters = document.getElementById("shuffleFilters");
 
-shuffleFilters.addEventListener("click", async () => {
+docShuffleFilters.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: shuffle,
+    function: shuffleFilters,
   });
 });
 
-// shuffle quick filters
-function shuffle() {
+function shuffleFilters() {
   let parent = document.getElementById('js-work-quickfilters');
   if(!parent) return;
   let membersCount = parent.childElementCount - 3;
   getRandom = () => Math.floor(Math.random() * membersCount) + 1;
   for(let iteration = 1; iteration <= membersCount; iteration++) {
+    let node1 = parent.childNodes[iteration];
+    let node2 = parent.childNodes[getRandom()];
+    parent.insertBefore(node1, node2);
+ }
+}
+
+
+let docShuffleSwimLanes = document.getElementById("shuffleSwimLanes");
+
+docShuffleSwimLanes.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: shuffleSwimLanes,
+  });
+});
+
+function shuffleSwimLanes() {
+  let parent = document.getElementById('ghx-pool');
+  if(!parent) return;
+  let membersCount = parent.childElementCount;
+  getRandom = () => Math.floor(Math.random() * membersCount);
+  for(let iteration = 0; iteration <= membersCount; iteration++) {
     let node1 = parent.childNodes[iteration];
     let node2 = parent.childNodes[getRandom()];
     parent.insertBefore(node1, node2);
